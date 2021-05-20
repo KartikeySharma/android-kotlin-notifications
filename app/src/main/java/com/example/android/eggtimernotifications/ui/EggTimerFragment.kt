5eca4ca7509e9e1f82c4af8ed58df15eaf161b26
Kartikey Sharma <kartikey.bluebells@gmail.com>
2021-05-20 12:19:19 +0530

    Notification created

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -66,22 +66,21 @@ class EggTimerFragment : Fragment() {
                 channelId,
                 channelName,
                 // TODO: Step 2.4 change importance
-                NotificationManager.IMPORTANCE_HIGH
+                NotificationManager.IMPORTANCE_LOW
             )// TODO: Step 2.6 disable badges for this channel
                 .apply {
                     setShowBadge(false)
                 }
 
             notificationChannel.enableLights(true)
-            notificationChannel.lightColor = Color.RED
+            notificationChannel.lightColor = Color.GREEN
             notificationChannel.enableVibration(true)
             notificationChannel.description = getString(R.string.breakfast_notification_channel_description)
 
-            val notificationManager = requireActivity().getSystemService(
+            val currentNotification= requireActivity().getSystemService(
                 NotificationManager::class.java
             )
-            notificationManager.createNotificationChannel(notificationChannel)
-
+            currentNotification.createNotificationChannel(notificationChannel)
         }
         // TODO: Step 1.6 END create a channel
     }
--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerViewModel.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerViewModel.kt
@@ -119,6 +119,13 @@ class EggTimerViewModel(private val app: Application) : AndroidViewModel(app) {
                 }
                 val triggerTime = SystemClock.elapsedRealtime() + selectedInterval
 
+                // Send notification to app
+                val notificationmanager= ContextCompat.getSystemService(
+                    app,
+                    NotificationManager::class.java
+                ) as NotificationManager
+                notificationmanager.sendNotification(app.getString(R.string.timer_running), app)
+
                 // TODO: Step 1.15 call cancel notification
                 val notificationManager =
                     ContextCompat.getSystemService(
